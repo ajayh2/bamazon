00216520bedcab11d2bc0b09c2a933b7db4ee8f5
Ajay Singh <ajayh2@gmail.com>
2019-12-19 14:07:55 -0500

    Creating function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table2')
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -15,3 +16,22 @@ var connection = mysql.createConnection({
   password: "sandhu49",
   database: "bamazon"
 });
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log(
+    "Welcome to Bamazon " +
+      connection.threadId
+  );
+
+  itemList();
+});
+
+
+function itemsList(conn) {
+  conn.query("Select * from products;", (err, res) => {
+      if (err) console.log("Error is: ", err + "\n");
+      console.log("Here is what we currently have in stock: \n\n");
+      console.table(res);
+      displayChoices(conn);
+  });
+} 
